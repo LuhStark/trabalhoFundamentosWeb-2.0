71be89bc06dbb354494b68b1c7057b31e1730a4e
Luuh Stark <81439785+LuhStark@users.noreply.github.com>
2023-07-11 18:34:58 -0300

    json e alguns troços

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
       document.getElementById("myForm").reset(); // Limpa os campos do formulário
       setTimeout(function(){
         window.location.href = "index.html"
-      },3000);
+      },10000);
     }
   });
 
+
+   
   function validateForm() {
     var nome = document.getElementById("nome").value;
     var motivo_contato = document.getElementById("motivo_contato").value;
@@ -21,7 +23,11 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
       alert("Por favor, preencha todos os campos.");
       return false;
     }
-
+    document.getElementById("nome2").value = nome;
+    document.getElementById("contato2").value = motivo_contato;
+    document.getElementById("email2").value = email;
+    document.getElementById("celular2").value = celular;
+    console.log(nome )
     return true;
   }
 
